0a21398763221837149f7c054aa02ebc11e991b9
Noa <nducrot@gaming.tech>
2024-04-05 16:53:21 +0200

    j'ai remis à jour les collision des ennemis pour faire demi tour au bout de la plateforme sur laquel il se trouve et j'ai aussi clean pour ne laisser que mes ajouts sur les monstres

--- ajoutEnnemis/ennemi.py
+++ ajoutEnnemis/ennemi.py
@@ -18,74 +18,6 @@ LEFT = (-1, 0)
 RIGHT = (1, 0)
 
 
-def distance(x, y):
-    return ((x[0] - y[0]) ** 2 + (x[1] - y[1]) ** 2) ** 0.5
-
-def Filtre(surface, transparence, rayon, objet):
-    filtre_noire = pygame.Surface(surface.get_size(), pygame.SRCALPHA)
-    filtre_noire.fill((0, 0, 0, 60)) ##le filtre autour de l'objet
-    
-    surface.blit(filtre_noire, (0, 0))
-    
-    if objet is not None: ##si appliqué autour d'un objet
-        filtre_objet = pygame.Surface(surface.get_size(), pygame.SRCALPHA) 
-        filtre_objet.fill((0, 0, 0, transparence//2)) ##va appliqué ce filtre tout autour de l'objet
-        pygame.draw.circle(filtre_objet, (0, 0, 0, 0), objet, rayon) #la taille du cercle de limiere autour de l'objet
-        surface.blit(filtre_objet, (0, 0)) ##affiche le filtre
-    else:
-        filtre_noire = pygame.Surface(surface.get_size(), pygame.SRCALPHA)
-        filtre_noire.fill((0, 0, 0, transparence))
-        surface.blit(filtre_noire, (0, 0))
-
-
-class Snake:
-    def __init__(self):
-        self.length = 1
-        self.position = [screen_width // 2, screen_height // 2]
-        self.direction = RIGHT
-        self.rect = pygame.Rect(self.position, (grid_size,grid_size))
-
-    def turn(self, point):
-        self.direction = point
-
-    def move(self):
-        current = self.position
-        x, y = self.direction
-        new = (current[0] + x * grid_size), (current[1] + y * grid_size)
-        self.position = new
-        self.rect = pygame.Rect(self.position, (grid_size,grid_size))
-
-    def draw(self, surface):
-        pygame.draw.rect(surface, (0,255,0), self.rect)
-
-    def handle_keys(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                return
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    self.turn(UP)
-                if event.key == pygame.K_DOWN:
-                    self.turn(DOWN)
-                if event.key == pygame.K_RIGHT:
-                    self.turn(RIGHT)
-                if event.key == pygame.K_LEFT:
-                    self.turn(LEFT)
-                    
-
-class Fruit:
-    def __init__(self):
-        self.position = (20,20)
-        self.rect = pygame.Rect(self.position, (grid_size,grid_size))
-
-    def random_position(self):
-        self.position = ((random.randint(0, grid_width - 1) * grid_size),(random.randint(0, grid_height - 1) * grid_size))
-        self.rect = pygame.Rect(self.position, (grid_size,grid_size))
-   
-    def draw(self, surface):
-        pygame.draw.rect(surface, (0,0,255), self.rect)
-
 
 class Projectiles:
     def __init__(self, x, y, width, height, direction):
@@ -107,7 +39,6 @@ class Projectiles:
     def draw(self, screen):
         pygame.draw.rect(screen, (255, 0, 0), self.rect)
 
-
 class Ennemi(pygame.sprite.Sprite):
     def __init__(self, x, y, width, height, image_path):
         super().__init__()
@@ -123,8 +54,8 @@ class Ennemi(pygame.sprite.Sprite):
         self.speed = 2
         self.direction = RIGHT
 
-    def update(self, snake):
-        dx = snake.position[0] - self.rect.x
+    def update(self, perso):
+        dx = perso.position[0] - self.rect.x
         if dx < 0:
             side = 'left'
         elif dx > 0:
@@ -144,10 +75,9 @@ class Ennemi(pygame.sprite.Sprite):
             self.rect.x += self.speed
         elif self.direction == LEFT:
             self.rect.x -= self.speed
-
         
-    def collision(self):
-        if self.rect.right >= screen_width or self.rect.left <= 0: ##a modifier selon les éléments de collisions
+    def collision(self, plateforme):
+        if self.rect.right >= screen_width or self.rect.left <= 0 or self.rect.right >= plateforme.rect.right or self.rect.left <= plateforme.rect.left: ##a modifier selon les éléments de collisions
             if self.direction == RIGHT:
                 self.direction = LEFT
             elif self.direction == LEFT:
@@ -160,8 +90,8 @@ class Ennemi(pygame.sprite.Sprite):
         else:
             surface.blit(self.image, self.rect)
 
-def tirer(snake, ennemi):
-    ecart = math.hypot(ennemi.rect.x - snake.position[0], ennemi.rect.y - snake.position[1])
+def tirer(perso, ennemi):
+    ecart = math.hypot(ennemi.rect.x - perso.position[0], ennemi.rect.y - perso.position[1])
 
     if ecart <= 100:
         if ennemi.side == "left":
@@ -181,9 +111,6 @@ def main():
     pygame.display.set_caption("Snake")
 
     clock = pygame.time.Clock()
-    opacity = 225
-    snake = Snake()
-    fruit = Fruit()
     ennemiTir = Ennemi(160, 160, 40, 40, "SpritemonstreExemple.png")
     ennemiCourt = Ennemi(160, 280, 40, 40, "SpritemonstreExemple.png")
     
@@ -193,19 +120,14 @@ def main():
     while True:
 
         screen.fill((255,255,255))
-        snake.handle_keys()
-        snake.move()
-        snake.draw(screen)
-        fruit.draw(screen)
         ennemiCourt.draw(screen)
         ennemiTir.draw(screen)
-        Filtre(screen, opacity, 50, snake.rect.center)
         
         ennemiCourt.deplacer()
-        ennemiCourt.collision()
+        ennemiCourt.collision("plateforme") #l'objet plateforme sur lequel il se trouve 
         
         if time.time() - projectile_timer >= 2:
-            tir = tirer(snake, ennemiTir)
+            tir = tirer("perso", ennemiTir) #mettre notre personnage en tant qu'attribut
             if tir:
                 projectiles.append(tir)
                 projectile_timer = time.time()
@@ -217,11 +139,7 @@ def main():
             if tir.x < 0 or tir.x > screen_width or tir.y < 0 or tir.y > screen_height:
                 projectiles.remove(tir)
 
-        if snake.rect.colliderect(fruit.rect):
-            opacity -= 40
-            fruit.random_position()
-
-        ennemiTir.update(snake)
+        ennemiTir.update("perso") 
         pygame.display.update()
         clock.tick(10)
 
